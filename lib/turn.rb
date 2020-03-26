afd82a79cb4d9618bd6d44a196e66c3ea1b4bf43
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-26 17:59:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,9 +38,7 @@ def turn(board)
   puts "Please enter 1-9:"
   index = gets.to_i - 1
   input_to_index(index)
-  if valid_move?(board, index) == false
-    puts turn(board)
-  else valid_move?(board, index) == true
+  valid_move?(board, index) == true
     move(board, index)
     display_board(board)
   end
